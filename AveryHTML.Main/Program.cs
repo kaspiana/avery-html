cfebcfad0115a18c540afb6e467487ea129e6e1c
kaspiana <fmcardiac@gmail.com>
2024-07-02 08:49:12 +1000

    add: node types with basic funcs

--- AveryHTML.Main/Program.cs
+++ AveryHTML.Main/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using AveryHTML;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var doc = new DocumentNode([
+
+    new ElementNode(
+        "div",
+        [("id", "main_div")],
+        [
+            new DataNode("Hello World.")
+        ]
+    )
+
+]);
+
+Console.WriteLine(doc.Render());
